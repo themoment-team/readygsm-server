8dddd39494465c8baab6cd596d085f1e20b9bca8
snowykte0426 <snowykte0426@naver.com>
2025-07-01 21:09:56 +0900

    init: RedayGSM 프로젝트 시작합니다🎉

---
+++ src/main/java/team/themoment/readygsm/ReadygsmApplication.java
@@ -0,0 +1,13 @@
+package team.themoment.readygsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadygsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadygsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/themoment/readygsm/ReadygsmApplicationTests.java
@@ -0,0 +1,9 @@
+package team.themoment.readygsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadygsmApplicationTests {
+
+}
