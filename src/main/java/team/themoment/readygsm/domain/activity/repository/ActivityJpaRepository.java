7835a9874d99b263dce832f941b75567d15032d7
snowykte0426 <snowykte0426@naver.com>
2025-07-08 21:12:20 +0900

    feat(activity): 활동 도메인 관련 Entity,Repository,Enum 등 생성

---
+++ src/main/java/team/themoment/readygsm/domain/activity/repository/ActivityJpaRepository.java
@@ -0,0 +1,9 @@
+package team.themoment.readygsm.domain.activity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team.themoment.readygsm.domain.activity.entity.ActivityJpaEntity;
+
+@Repository
+public interface ActivityJpaRepository extends JpaRepository<ActivityJpaEntity, Integer> {
+}
