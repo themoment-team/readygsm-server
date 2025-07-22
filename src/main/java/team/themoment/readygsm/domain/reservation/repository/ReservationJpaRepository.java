a5beeaad7a7766aab6776b191e28ab3dc8cd9971
snowykte0426 <snowykte0426@naver.com>
2025-07-22 17:31:03 +0900

    fix(user): 사용자 조회 시 JPA N+1 문제 해결

--- src/main/java/team/themoment/readygsm/domain/reservation/repository/ReservationJpaRepository.java
+++ src/main/java/team/themoment/readygsm/domain/reservation/repository/ReservationJpaRepository.java
@@ -1,6 +1,7 @@
 package team.themoment.readygsm.domain.reservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import team.themoment.readygsm.domain.reservation.entity.ReservationJpaEntity;
 
@@ -8,5 +9,8 @@ import java.util.List;
 
 @Repository
 public interface ReservationJpaRepository extends JpaRepository<ReservationJpaEntity, Long> {
+    @Query("SELECT r FROM ReservationJpaEntity r JOIN FETCH r.activity WHERE r.user.id = :userId")
     List<ReservationJpaEntity> findByUserId(Long userId);
+    @Query("SELECT r FROM ReservationJpaEntity r JOIN FETCH r.activity WHERE r.user.id IN :userIds")
+    List<ReservationJpaEntity> findByUserIdIn(List<Long> userIds);
 }
