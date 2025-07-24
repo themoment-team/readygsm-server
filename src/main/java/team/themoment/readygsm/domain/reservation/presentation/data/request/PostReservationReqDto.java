8817dde3985a4c53472b46cdbeb5d596fc0c37c8
ZaMan <roblery128@gmail.com>
2025-07-24 18:46:23 +0900

    add dependencies

--- src/main/java/team/themoment/readygsm/domain/reservation/presentation/data/request/PostReservationReqDto.java
+++ src/main/java/team/themoment/readygsm/domain/reservation/presentation/data/request/PostReservationReqDto.java
@@ -1,10 +1,11 @@
 package team.themoment.readygsm.domain.reservation.presentation.data.request;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 
 public record PostReservationReqDto(
-        String phoneNumber,
-        String applicantName,
+        @NotNull String phoneNumber,
+        @NotNull String applicantName,
         String schoolName,
         Integer grade,
         Integer classNumber,
