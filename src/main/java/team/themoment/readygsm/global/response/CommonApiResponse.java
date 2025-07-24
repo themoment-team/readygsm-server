2db8c52ee02cea82338dd69e483f725b73dd17d1
snowykte0426 <snowykte0426@naver.com>
2025-07-24 11:32:15 +0900

    feat(global): CommonApiResponse 설정 추가

---
+++ src/main/java/team/themoment/readygsm/global/response/CommonApiResponse.java
@@ -0,0 +1,37 @@
+package team.themoment.readygsm.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nonnull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonApiResponse<T> {
+
+    @Schema(description = "상태 메시지", nullable = false, example = "OK")
+    HttpStatus status;
+    @Schema(description = "상태 코드", nullable = false, example = "200")
+    int code;
+    @Schema(description = "메시지", nullable = false, example = "완료되었습니다.")
+    String message;
+    @Schema(description = "데이터", nullable = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    T data;
+
+    public static CommonApiResponse success(@Nonnull String message) {
+        return new CommonApiResponse(HttpStatus.OK, HttpStatus.OK.value(), message, null);
+    }
+
+    public static CommonApiResponse created(@Nonnull String message) {
+        return new CommonApiResponse(HttpStatus.CREATED, HttpStatus.CREATED.value(), message, null);
+    }
+
+    public static CommonApiResponse error(@Nonnull String message, @Nonnull HttpStatus status) {
+        return new CommonApiResponse(status, status.value(), message, null);
+    }
+}
