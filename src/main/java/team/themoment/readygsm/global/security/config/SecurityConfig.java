78a110ec1b9312840e4a3400ab9274a9a2ee8dae
hongjm0912~ <s25036@gsm.hs.kr>
2025-07-28 14:27:57 +0900

    인증/인가 구현

--- src/main/java/team/themoment/readygsm/global/security/config/SecurityConfig.java
+++ src/main/java/team/themoment/readygsm/global/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import team.themoment.readygsm.global.security.jwt.JwtAuthenticationFilter;
@@ -30,6 +31,10 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
 
         .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/login/**").permitAll()
