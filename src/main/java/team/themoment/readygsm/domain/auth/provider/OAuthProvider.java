1a7cb8d1d411a437276dbd558d37ed480d3bc491
hongjm0912~ <s25036@gsm.hs.kr>
2025-08-03 19:44:52 +0900

    update(auth): OAuthProvider 코드 수정

--- src/main/java/team/themoment/readygsm/domain/auth/provider/OAuthProvider.java
+++ src/main/java/team/themoment/readygsm/domain/auth/provider/OAuthProvider.java
@@ -1,6 +1,7 @@
 package team.themoment.readygsm.domain.auth.provider;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import team.themoment.readygsm.global.security.oauth.OAuthUserInfo;
 
 import java.util.HashMap;
@@ -9,12 +10,37 @@ import java.util.Map;
 @Component
 public class OAuthProvider {
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
     public OAuthUserInfo getUserInfo(String code) {
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("email", "test@example.com");
-        attributes.put("name", "홍길동");
+
+        String accessToken = requestAccessToken(code);
+
+        Map<String, Object> attributes = requestUserAttributes(accessToken);
+
+        attributes.put("role", determineRole(attributes));
 
         return new OAuthUserInfo(attributes);
     }
+
+    private String requestAccessToken(String code) {
+           return "mock_access_token";
+    }
+
+    private Map<String, Object> requestUserAttributes(String accessToken) {
+        Map<String, Object> attributes = new HashMap<>();
+
+        attributes.put("email", "test@example.com");
+        attributes.put("name", "홍길동");
+
+        return attributes;
+    }
+
+    private String determineRole(Map<String, Object> attributes) {
+        String email = (String) attributes.get("email");
+        if (email.endsWith("@school.edu")) return "TEACHER";
+        return "USER";
+    }
 }
+
 
