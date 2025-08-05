c4272b66443fd3af9feddd6ba9337951063e5dae
ZaMan <roblery128@gmail.com>
2025-08-05 15:29:29 +0900

    chore(reservation): Dto 클래스 import 위치 변경

--- src/main/java/team/themoment/readygsm/domain/reservation/presentation/data/response/PostUserReservationResDto.java
+++ src/main/java/team/themoment/readygsm/domain/reservation/presentation/data/response/PostUserReservationResDto.java
@@ -1,6 +1,6 @@
 package team.themoment.readygsm.domain.reservation.presentation.data.response;
 
-import team.themoment.readygsm.domain.reservation.presentation.data.dto.PostUserReservationActivityDto;
+import team.themoment.readygsm.domain.reservation.presentation.data.PostUserReservationActivityDto;
 
 public record PostUserReservationResDto(
         Long reservationId,
