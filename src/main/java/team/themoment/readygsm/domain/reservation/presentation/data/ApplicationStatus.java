6f13fb96001bf9bcaf0637992164fbfab4b7eb63
ZaMan <roblery128@gmail.com>
2025-08-10 01:48:17 +0900

    refactor(reservation): Enum 명 변경

--- src/main/java/team/themoment/readygsm/domain/reservation/presentation/data/ApplicationStatus.java
+++
@@ -1,7 +0,0 @@
-package team.themoment.readygsm.domain.reservation.presentation.data;
-
-public enum ApplicationStatus {
-    UPCOMING,
-    CLOSED,
-    OPEN
-}
--- src/main/java/team/themoment/readygsm/domain/reservation/service/SearchReservationService.java
+++ src/main/java/team/themoment/readygsm/domain/reservation/service/SearchReservationService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.themoment.readygsm.domain.reservation.data.Reservation;
 import team.themoment.readygsm.domain.reservation.entity.ReservationJpaEntity;
-import team.themoment.readygsm.domain.reservation.presentation.data.ApplicationStatus;
+import team.themoment.readygsm.domain.reservation.presentation.data.constant.ActivityStatus;
 import team.themoment.readygsm.domain.reservation.presentation.data.SearchReservationActivityDto;
 import team.themoment.readygsm.domain.reservation.presentation.data.response.SearchReservationResDto;
 import team.themoment.readygsm.domain.reservation.repository.ReservationJpaRepository;
@@ -65,14 +65,14 @@ public class SearchReservationService {
     // 활동 상태
     private String getApplicationStatus(LocalDateTime startTime, LocalDateTime endTime) {
         LocalDateTime now = LocalDateTime.now();
-        ApplicationStatus status;
+        ActivityStatus status;
 
         if(now.isBefore(startTime)) {
-            status = ApplicationStatus.UPCOMING;
+            status = ActivityStatus.UPCOMING;
         } else if(now.isAfter(endTime)) {
-            status = ApplicationStatus.CLOSED;
+            status = ActivityStatus.CLOSED;
         } else {
-            status = ApplicationStatus.OPEN;
+            status = ActivityStatus.OPEN;
         }
         return status.name();
     }
