67c8e1bd401b7a75513907f70e77a80ea7f87b99
hongjm0912~ <s25036@gsm.hs.kr>
2026-04-10 12:01:42 +0900

    add(auth): OAuth2 커스텀 유저 서비스, 성공/실패 핸들러 추가

---
+++ src/main/java/team/themoment/readygsmserver/global/security/oauth2/OAuth2FailureHandler.java
@@ -0,0 +1,19 @@
+package team.themoment.readygsmserver.global.security.oauth2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+    }
+}
---
+++ src/main/java/team/themoment/readygsmserver/global/security/oauth2/OAuth2SuccessHandler.java
@@ -0,0 +1,44 @@
+package team.themoment.readygsmserver.global.security.oauth2;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import team.themoment.readygsmserver.global.security.jwt.JwtProvider;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = oAuth2User.getAttribute("email");
+
+        String accessToken = jwtProvider.createAccessToken(email);
+        String refreshToken = jwtProvider.createRefreshToken(email);
+
+        // TODO: Refresh Token Redis 저장 로직 추가
+
+        response.addCookie(createCookie("access_token", accessToken));
+        response.addCookie(createCookie("refresh_token", refreshToken));
+
+        getRedirectStrategy().sendRedirect(request, response, "/");
+    }
+
+    private Cookie createCookie(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        return cookie;
+    }
+}
