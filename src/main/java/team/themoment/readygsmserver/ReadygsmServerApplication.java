6fe57b72a9ab7e1962d2c1f6a18dee03a9ed2d31
hongjm0912~ <s25036@gsm.hs.kr>
2026-04-10 09:44:27 +0900

    init: 기존 프로잭트 삭제

---
+++ src/main/java/team/themoment/readygsmserver/ReadygsmServerApplication.java
@@ -0,0 +1,13 @@
+package team.themoment.readygsmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadygsmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadygsmServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/themoment/readygsmserver/ReadygsmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.themoment.readygsmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadygsmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
