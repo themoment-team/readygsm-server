6cc47bd37adf0b7bf3e3c6c09a4497e78421146e
zaman <roblery128@gmail.com>
2026-04-20 14:29:21 +0900

    refactor(user): Role Enum을 GrantedAuthority에서 확장

--- src/main/java/team/themoment/readygsmserver/domain/user/entity/constant/Role.java
+++ src/main/java/team/themoment/readygsmserver/domain/user/entity/constant/Role.java
@@ -1,6 +1,13 @@
 package team.themoment.readygsmserver.domain.user.entity.constant;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER,
-    ADMIN
+    ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name();
+    }
 }
