3f6ccf39d0b09fa1c93c214a0b30f9e1b7c729e2
hongjm0912~ <s25036@gsm.hs.kr>
2026-05-06 23:34:14 +0900

    refactor(user): SecurityContextHolder 제거 및 @AuthenticationPrincipal로 교체
    서비스 레이어에서 SecurityContextHolder를 직접 사용하는 대신
    컨트롤러에서 @AuthenticationPrincipal로 OAuth2User를 주입받아 서비스에 전달하도록 변경

--- src/main/java/team/themoment/readygsmserver/domain/user/controller/UserController.java
+++ src/main/java/team/themoment/readygsmserver/domain/user/controller/UserController.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +28,7 @@ public class UserController {
             @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자")
     })
     @GetMapping("/me")
-    public ResponseEntity<UserResDto> getMe() {
-        return ResponseEntity.ok(userService.getMe());
+    public ResponseEntity<UserResDto> getMe(@AuthenticationPrincipal OAuth2User user) {
+        return ResponseEntity.ok(userService.getMe(user));
     }
 }
--- src/main/java/team/themoment/readygsmserver/domain/user/service/UserService.java
+++ src/main/java/team/themoment/readygsmserver/domain/user/service/UserService.java
@@ -1,8 +1,5 @@
 package team.themoment.readygsmserver.domain.user.service;
 
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import team.themoment.readygsmserver.domain.user.dto.response.UserResDto;
@@ -10,17 +7,10 @@ import team.themoment.readygsmserver.domain.user.dto.response.UserResDto;
 @Service
 public class UserService {
 
-    public UserResDto getMe() {
-        var authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication instanceof OAuth2AuthenticationToken token) {
-            OAuth2User user = token.getPrincipal();
-            Long id = ((Number) user.getAttribute("id")).longValue();
-            String email = user.getAttribute("email");
-            String role = user.getAttribute("role");
-            return new UserResDto(id, email, role);
-        }
-
-        throw new AccessDeniedException("인증 정보가 없습니다.");
+    public UserResDto getMe(OAuth2User user) {
+        Long id = ((Number) user.getAttribute("id")).longValue();
+        String email = user.getAttribute("email");
+        String role = user.getAttribute("role");
+        return new UserResDto(id, email, role);
     }
 }
