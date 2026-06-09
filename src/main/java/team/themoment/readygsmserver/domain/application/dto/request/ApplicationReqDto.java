ddb772f51de3eade022566c461c6df67b1e5f4aa
zaman <roblery128@gmail.com>
2026-06-09 21:05:36 +0900

    refactor(application): 반과 번호 최대 증가

--- src/main/java/team/themoment/readygsmserver/domain/application/dto/request/ApplicationReqDto.java
+++ src/main/java/team/themoment/readygsmserver/domain/application/dto/request/ApplicationReqDto.java
@@ -9,8 +9,8 @@ import jakarta.validation.constraints.Size;
 public record ApplicationReqDto(
         @NotBlank @Size(max = 10) String name,
         @Min(1) @Max(3) Integer grade,
-        @Min(1) @Max(4) Integer classNumber,
-        @Min(1) @Max(30) Integer number,
+        @Min(1) @Max(20) Integer classNumber,
+        @Min(1) @Max(50) Integer number,
         @NotBlank @Size(max = 200) String schoolName,
         @NotBlank @Pattern(regexp = "^01[0-9]-\\d{3,4}-\\d{4}$") String phoneNumber,
         @NotBlank @Pattern(regexp = "^01[0-9]-\\d{3,4}-\\d{4}$") String familyPhoneNumber
